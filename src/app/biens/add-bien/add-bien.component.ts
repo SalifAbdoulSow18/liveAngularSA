64b871e5adf5f0941be5d67eac716b1769381529
SalifAbdoulSow18 <abdoulsalifsow06@gmail.com>
2022-08-31 15:33:01 +0000

    mis a jour

--- src/app/biens/add-bien/add-bien.component.ts
+++ src/app/biens/add-bien/add-bien.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add-bien',
@@ -14,14 +14,33 @@ export class AddBienComponent implements OnInit {
 
   ngOnInit(): void {
     // creating the form
+    // @ts-ignore
     this.bienForm = new FormGroup({
-      title: new FormControl(null),
-      price: new FormControl(null),
-      description: new FormControl(null),
+      title: new FormControl(null,
+        [Validators.required,
+          Validators.minLength(5), Validators.maxLength(50)]),
+      price: new FormControl(null,
+        [Validators.required, Validators.min(1000)]),
+      description: new FormControl(null, Validators.required),
       category: new FormControl(null), // select
-      image: new FormControl(null),
+      image: new FormControl(null, Validators.required),
       sale: new FormControl('Location'), // radio
+      imageSec: new FormArray([])
     });
+    this.bienForm.patchValue({
+      sale: 'vente'
+    });
+  }
+  // validation function
+  // tslint:disable-next-line:typedef
+  // tslint:disable-next-line:typedef
+  getImageSecControls() {
+    return (this.bienForm.get('imageSec') as FormArray).controls;
+  }
+  // tslint:disable-next-line:typedef
+  onAddSecImage(){
+    const control = new FormControl(null, Validators.required);
+    (this.bienForm.get('imageSec') as FormArray).push(control);
   }
 
 }
